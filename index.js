4cf3aa4e063239bee87db1407377ddc109ed9209
Ehsan Ziya <ehsan.ziya@gmail.com>
2015-09-07 21:45:51 +0100

    Feature: New audio file

--- index.js
+++ index.js
@@ -73,7 +73,7 @@ function audioEnded() {
   });
 }
 
-loadSound(context, './audio/zya2.mp3', function (audiobuffer) {
+loadSound(context, './audio/zya-swirl.mp3', function (audiobuffer) {
   buffer = audiobuffer;
 
   play.style.visibility = 'visible';
